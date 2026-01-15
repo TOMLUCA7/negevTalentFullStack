7114ca5a5728dd61789a4d4276a1a116d07b2a7b
TOMLUCA7 <your-github-email@example.com>
2026-01-15 18:07:13 +0200

    chore: Comment out all existing code examples in `index.js`.

--- js-script/time-complexity/index.js
+++ js-script/time-complexity/index.js
@@ -1,100 +1,184 @@
 // 1
-const getBalance = function (bankOperations) {
-  let balance = 0;
-  for (let op of bankOperations) {
-    balance += op;
-  }
-  return balance;
-};
+// const getBalance = function (bankOperations) {
+//   let balance = 0;
+//   for (let op of bankOperations) {
+//     balance += op;
+//   }
+//   return balance;
+// };
 
-getBalance([-27, -43, -2400, -700, 15000, 58]);
+// getBalance([-27, -43, -2400, -700, 15000, 58]);
 // run time O(n)
 
 // 2
-const printSome = function (complaints) {
-  for (let i = 1; i < complaints.length; i = i * 2)
-    console.log(complaints[i].text);
-};
+// const printSome = function (complaints) {
+//   for (let i = 1; i < complaints.length; i = i * 2)
+//     console.log(complaints[i].text);
+// };
 // run time O(log n)
 
 // 3
-const allSides = [
-  { a: 3, b: 4 },
-  { a: 15, b: 21 },
-  { a: 41, b: 8 },
-  { a: 12, b: 6 },
-];
+// const allSides = [
+//   { a: 3, b: 4 },
+//   { a: 15, b: 21 },
+//   { a: 41, b: 8 },
+//   { a: 12, b: 6 },
+// ];
 
-const relevantSides = allSides.filter((s) => s.a % 3 == 0);
-for (let sides of relevantSides) {
-  console.log(getHype(sides));
-}
+// const relevantSides = allSides.filter((s) => s.a % 3 == 0);
+// for (let sides of relevantSides) {
+//   console.log(getHype(sides));
+// }
 
-const getHype = function (sides) {
-  let a = sides.a;
-  let b = sides.b;
-  let sumOfSquares = a * a + b * b;
-  return Math.sqrt(sumOfSquares);
-};
+// const getHype = function (sides) {
+//   let a = sides.a;
+//   let b = sides.b;
+//   let sumOfSquares = a * a + b * b;
+//   return Math.sqrt(sumOfSquares);
+// };
 // run time O(n)
 
 // 4
 // What is the complexity of the getDistributions function?
-const studentAnswers = {
-  brBlds: {
-    1: "a",
-    2: "a",
-    3: "c",
-  },
-  dvOna: {
-    1: "a",
-    2: "c",
-    3: "c",
-  },
-  nmPrz: {
-    1: "a",
-    2: "b",
-    3: "a",
-  },
-};
-const studentGithubs = ["brBlds", "dvOna", "nmPrz"];
+// const studentAnswers = {
+//   brBlds: {
+//     1: "a",
+//     2: "a",
+//     3: "c",
+//   },
+//   dvOna: {
+//     1: "a",
+//     2: "c",
+//     3: "c",
+//   },
+//   nmPrz: {
+//     1: "a",
+//     2: "b",
+//     3: "a",
+//   },
+// };
+// const studentGithubs = ["brBlds", "dvOna", "nmPrz"];
 
-const distributions = {
-  1: {},
-  2: {},
-  3: {},
-};
+// const distributions = {
+//   1: {},
+//   2: {},
+//   3: {},
+// };
 
-const getDistributions = function (studentAnswers) {
-  studentGithubs.forEach((sg) => {
-    let answers = studentAnswers[sg];
+// const getDistributions = function (studentAnswers) {
+//   studentGithubs.forEach((sg) => {
+//     let answers = studentAnswers[sg];
 
-    Object.keys(answers).forEach((questionNumber) => {
-      let letterAnswer = answers[questionNumber];
+//     Object.keys(answers).forEach((questionNumber) => {
+//       let letterAnswer = answers[questionNumber];
 
-      distributions[questionNumber][letterAnswer]
-        ? distributions[questionNumber][letterAnswer]++
-        : (distributions[questionNumber][letterAnswer] = 1);
-    });
-  });
+//       distributions[questionNumber][letterAnswer]
+//         ? distributions[questionNumber][letterAnswer]++
+//         : (distributions[questionNumber][letterAnswer] = 1);
+//     });
+//   });
 
-  return distributions;
-};
+//   return distributions;
+// };
 
-getDistributions(studentAnswers);
+// getDistributions(studentAnswers);
 // run time O(n)
 
 // 5
-const sendEmails = (email, recepients) =>
-  recepients.forEach((r) => r.sendEmail(email));
+// const sendEmails = (email, recepients) =>
+//   recepients.forEach((r) => r.sendEmail(email));
 
-const emailManager = function () {
-  let email = generateEmail();
+// const emailManager = function () {
+//   let email = generateEmail();
 
-  $.get("/recepients", function (recepients) {
-    sendEmails(email, recepients);
-  });
-};
+//   $.get("/recepients", function (recepients) {
+//     sendEmails(email, recepients);
+//   });
+// };
 // run time O(n)
 
 // 6
+
+// const findDuplicates = (arr) => {
+//   const memory = new Set();
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (memory.has(arr[i])) {
+//       console.log("find Duplicates");
+//       return;
+//     }
+//     memory.add(arr[i]);
+//   }
+//   console.log("Did Not find Duplicates");
+// };
+
+// console.log(findDuplicates([1, 2, 3, 3]));
+
+// returns the duplicates arr
+// const findDuplicates = (arr) => {
+//   const memory = new Set();
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (memory.has(arr[i])) {
+//       result.push(arr[i]);
+//     }
+//     memory.add(arr[i]);
+//   }
+//   return result;
+// };
+
+// console.log(findDuplicates([1, 2, 3, 3]));
+
+// 7
+// const employeesData = {
+//   ax01: 1300,
+//   qs84: 840,
+//   bg33: 2700,
+// };
+
+// const findEmployeeSalary = (employeeID) => {
+//   return employeesData[employeeID] || "Employee not found";
+// };
+
+// console.log(findEmployeeSalary("ax01"));
+// console.log(findEmployeeSalary("bg33"));
+// console.log(findEmployeeSalary("unknown_id"));
+
+// 8
+// let numbers = [
+//   24, 33, 66, 102, 108, 140, 146, 177, 182, 217, 341, 357, 372, 390, 418, 427,
+//   442, 444, 469, 480, 572, 624, 627, 665, 680, 694, 743, 768, 790, 794, 852,
+//   896, 919, 942, 982, 991, 1026, 1055, 1086, 1137, 1141, 1157, 1167, 1271, 1272,
+//   1273, 1301, 1337, 1340, 1344, 1388, 1455, 1465, 1466, 1509, 1555, 1640, 1667,
+//   1667, 1689, 1824, 1897, 1928, 1950, 1987, 2056, 2059, 2070, 2123, 2140, 2198,
+//   2215, 2260, 2304, 2383, 2403, 2433, 2454, 2472, 2480, 2481, 2535, 2543, 2554,
+//   2557, 2580, 2630, 2634, 2671, 2745, 2792, 2839, 2849, 2871, 2873, 2893, 2932,
+//   2962, 2984, 2987,
+// ];
+
+// const findIndex = (numbers, num) => {
+//   let low = 0;
+//   let high = numbers.length - 1;
+
+//   while (high >= low) {
+//     let mid = Math.floor((low + high) / 2);
+
+//     if (numbers[mid] === num) return mid;
+
+//     if (numbers[mid] > num) {
+//       high = mid - 1;
+//     } else {
+//       low = mid + 1;
+//     }
+//   }
+
+//   return -1;
+// };
+
+// console.log(findIndex(numbers, 2630));
+
+// 9
+// green -  O(1)
+// blue -  O(n)
+// yellow - O(log n)
+// brown - O(n2)
