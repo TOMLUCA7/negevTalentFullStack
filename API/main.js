e8e2369450a84079929609de0642cad8daa27ac4
TOMLUCA7 <your-github-email@example.com>
2026-01-28 13:25:33 +0200

    feat: Implement book rendering functionality by ISBN and title, replacing the previous random joke display.

--- API/main.js
+++ API/main.js
@@ -1,11 +1,45 @@
-const api = "https://official-joke-api.appspot.com/random_joke";
+// const api = "https://official-joke-api.appspot.com/random_joke";
 
-const joke = () => {
-  fetch(api)
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data)
-      const joke = document.getElementById("joke");
-      joke.textContent = `${data.setup}  punchline => ${data.punchline}`;
+// const joke = () => {
+//   fetch(api)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // console.log(data)
+//       const joke = document.getElementById("joke");
+//       joke.textContent = `${data.setup}  punchline => ${data.punchline}`;
+//     });
+// };
+
+// 1
+const Book_api = "https://www.googleapis.com/books/v1/volumes?q=";
+
+const renderBook = (isbn) => {
+  //   console.log(isbn);
+  axios
+    .get(Book_api + "isbn:" + isbn)
+    .then((response) => {
+      console.log(response.data.items[0]);
+      const book = document.getElementById("book");
+      book.textContent =
+        response.data.items[0].volumeInfo.title +
+        " by: " +
+        response.data.items[0].volumeInfo.authors[0];
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// 2
+const renderBookByTitle = (queryType, queryValue) => {
+  axios
+    .get(Book_api + queryType + ":" + queryValue)
+    .then((response) => {
+      console.log(response.data.items[0].volumeInfo.title);
+      const book = document.getElementById("bookByTitle");
+      book.textContent = response.data.items[0].volumeInfo.title;
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
